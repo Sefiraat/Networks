fff63d14d2659bd12b18cd44c13ca2e3c88cea54
JustAHuman-xD <65748158+JustAHuman-xD@users.noreply.github.com>
2024-10-19 10:50:25 -0500

    simple thing

--- src/main/java/io/github/sefiraat/networks/slimefun/network/NetworkQuantumStorage.java
+++ src/main/java/io/github/sefiraat/networks/slimefun/network/NetworkQuantumStorage.java
@@ -373,7 +373,8 @@ public class NetworkQuantumStorage extends SlimefunItem implements DistinctiveIt
     private static boolean isBlacklisted(@Nonnull ItemStack itemStack) {
         return itemStack.getType() == Material.AIR
             || itemStack.getType().getMaxDurability() < 0
-            || Tag.SHULKER_BOXES.isTagged(itemStack.getType());
+            || Tag.SHULKER_BOXES.isTagged(itemStack.getType())
+            || SlimefunItem.getByItem(itemStack) instanceof NetworkQuantumStorage;
     }
 
     @ParametersAreNonnullByDefault
